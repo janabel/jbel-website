2a880714140d25042f90e9102ba670ec6ceca799
janabel <janabeltxia@gmail.com>
2025-08-10 01:29:50 -0700

    update blog post list

--- src/components/Words.tsx
+++ src/components/Words.tsx
@@ -4,15 +4,21 @@ const Words = () => {
   return (
     <div className="page-text fade-in-text">
       <p>
-        i started writing (for the public) recently at{" "}
-        <a href="https://janabel.substack.com/">jbel's substack</a>! offloading
-        some refactored journal entries soon.
+        i started writing publicly at{" "}
+        <a href="https://janabel.substack.com/">jbel's substack</a>!
       </p>
-      <p>
-        here are my favorite posts right now (hopefully this section actually
-        makes sense one day):
-      </p>
+      <p>here are some of my posts:</p>
       <ul>
+        <li style={{ marginBottom: 0 }}>
+          <a href="https://janabel.substack.com/p/careful-optimism">
+            careful optimism | Jul 10, 2025
+          </a>
+        </li>
+        <li style={{ marginBottom: 0 }}>
+          <a href="https://janabel.substack.com/p/cryptography-and-social-values">
+            cryptography and social values | Dec 4, 2024
+          </a>
+        </li>
         <li style={{ marginBottom: 0 }}>
           <a href="https://janabel.substack.com/p/an-ode-to-dance">
             mosaic | Sep 2, 2024
