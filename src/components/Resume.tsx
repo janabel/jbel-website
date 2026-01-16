3729206f4ddb8cb65e93799183ab7ffe024e1a1f
janabel <janabeltxia@gmail.com>
2026-01-16 16:49:15 -0500

    testing resume PDF rendering

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -6,8 +6,21 @@ import ResumeContent from "../markdown/resume.mdx";
 
 const Resume = () => {
   return (
-    <div className="page-text fade-in-text">
-      <ResumeContent />
+    <div>
+      <div className="page-text fade-in-text">
+        <p>
+          A 2-page resume can be found{" "}
+          <a
+            href="/website_resume.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            here
+          </a>
+          .
+        </p>
+        <ResumeContent />
+      </div>
     </div>
   );
 };
