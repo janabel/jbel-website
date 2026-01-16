93276d880a9a9f5c97d9b7e9ef94e577331e14d6
janabel <janabeltxia@gmail.com>
2026-01-16 17:36:08 -0500

    fix resume PDF link locally

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ResumeContent from "../markdown/resume.mdx";
-// import resumePDF from "../assets/website_resume.pdf";
+import resumePDF from "../assets/website_resume.pdf";
 // import * as fs from "fs";
 
 // const resumeMarkdown = fs.readFileSync("../markdown/resume.mdx");
@@ -12,7 +12,8 @@ const Resume = () => {
         <p>
           A 2-page resume can be found{" "}
           <a
-            href="/website_resume.pdf"
+            // href="/website_resume.pdf"
+            href={resumePDF}
             target="_blank"
             rel="noopener noreferrer"
           >
